10105c05c73b023e7ee72c3e7acbb94a4e2cb568
myo-i <Imiyo0521@gmail.com>
2023-01-07 00:38:00 +0900

    ✨ デッドラインの設定

---
+++ creating-tcp-server/04_setDeadline/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(3 * time.Second))
+	if err != nil {
+		log.Fatalln("CONN TIMEOUT")
+	}
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+	defer conn.Close()
+
+	// SetDeadlineで設定した秒数経過すると強制終了
+	fmt.Println("***CODE GOT HERE***")
+}
